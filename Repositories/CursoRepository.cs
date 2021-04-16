6a806559e3d4040ed847b9e7b483303862d6c833
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-16 14:21:16 -0300

    Implementado os métodos da Interface

--- Repositories/CursoRepository.cs
+++ Repositories/CursoRepository.cs
@@ -1,9 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
 using data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Repositories.Contracts;
 
 namespace Repositories
 {
-    public class CursoRepository
+    public class CursoRepository : ICursoContract<Curso>
     {
          private Context _context= new Context();
+
+        public void DeleteCurso(int id)
+        {
+           var curso= _context.Cursos.Find(id);
+           _context.Remove(curso);
+            _context.SaveChanges();
+        }
+
+        public List<Curso> GetALL()
+        {
+            return _context.Cursos.ToList();
+        }
+
+        public Curso GetCurso(int id)
+        {
+            var curso= _context.Cursos.Find(id);
+            return curso;
+        }
+
+        public bool SaveCurso(Curso model)
+        {
+         
+           try
+           {
+                 _context.Cursos.Add(model);
+                 _context.SaveChanges();
+                 return true;
+               
+           }
+           catch 
+           {
+                return false;
+           }
+        }
+
+        public bool UpdateCurso(Curso model, int id)
+        {
+           try
+           {
+               model.Id = id;
+               _context.Entry<Curso>(model).State = EntityState.Modified;
+               _context.SaveChanges();
+               return true;
+           }
+           catch 
+           {
+                // TODO
+                return false;
+           }
+        }
     }
 }
