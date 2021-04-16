5b067e53755130c21f7b7465aed49306ac30a0fd
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-15 21:07:12 -0300

    Configurado o Modelo

--- Models/Formacao.cs
+++ Models/Formacao.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Models
 {
     public class Formacao
     {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Nome { get; set; }
+        
         
     }
 }
