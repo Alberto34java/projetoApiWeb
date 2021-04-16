65806597830a984b61f71551c9684b1edf534596
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-15 21:05:55 -0300

    Criado os atributos da classe

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Models
 {
     public class Curso
     {
+        [Key]
+        public int Id { get; set; }
+        
+        
+        [Required]
+        public string Nome { get; set; }
+        
         
     }
 }
