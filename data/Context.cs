145a471fb7ec2443d2bc6968a93c5e1b8a47cddc
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-16 11:54:38 -0300

    Configurado o arquivo de Contexto

--- data/Context.cs
+++ data/Context.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+
 namespace data
 {
-    public class Context 
+    public class Context : DbContext
     {
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+         optionsBuilder.UseSqlServer("Server=DESKTOP-0V20B0M\\SQLEXPRESS; Database=ApiCursoUniversity; User Id=AlbertoDevTeste; Password=asp02");
+        }
+
+       
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+       {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(Context).Assembly);
+       }
     }
 }
