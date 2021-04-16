3c7ad41f03cbe724f969a5fc8be611841f5b08ad
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-16 11:55:33 -0300

    Classe criada com algumas especificações

---
+++ data/Configuring/CursoConfigure.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+
+namespace data.Configuring
+{
+    public class CursoConfigure : IEntityTypeConfiguration<Curso>
+    {
+        public void Configure(EntityTypeBuilder<Curso> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.HasIndex(p => p.Nome).IsUnique();
+            builder.Property(p => p.Descricao).HasColumnType("VARCHAR(250)").IsRequired();
+            builder.Property(p => p.Duracao).HasColumnType("VARCHAR(20)").IsRequired();
+           
+        }
+    }
+}
